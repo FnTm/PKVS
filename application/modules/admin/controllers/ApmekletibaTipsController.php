d48961e1b2e8e6eb75a406782ed7bee9198e6b72
Janis Peisenieks <janis@peisenieks.lv>
2012-04-15 09:52:53 +0300

    Added apmeklejuma tipi, to add details for reasons of not attending

---
+++ application/modules/admin/controllers/ApmekletibaTipsController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: Janis
+ * Date: 12.15.4
+ * Time: 09:49
+ */
+ 
+class Admin_ApmekletibaTipsController extends JP_Controller_Action{
+    public function indexAction(){
+        
+    }
+
+}
